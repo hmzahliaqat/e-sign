7c7bf9bf114480c12c91e689fdff4bdf18a97e83
hmzah <hmzah.liaqat@gmail.com>
2025-02-27 14:47:14 -0500

    doc preview init

--- resources/js/Pages/Company/Pages/Documents/DocumentPreview.vue
+++ resources/js/Pages/Company/Pages/Documents/DocumentPreview.vue
@@ -1,6 +1,4 @@
-<template>
-<h1>Preview</h1>
-</template>
-
-
 <script setup></script>
+
+
+<template></template>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,31 @@
 
 use App\Http\Controllers\Company\CompanyController;
 use App\Http\Controllers\Document\DocumentController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 
 Route::get('/', [CompanyController::class , 'index']);
 Route::post('save/document', [DocumentController::class , 'upload']);
 Route::post('document/{id}/preview', [DocumentController::class , 'show']);
+
+
+Route::get('/secure-pdf/{filename}', function ($filename) {
+    // Ensure the file exists in the private documents folder
+    $path = $filename;
+
+    if (!Storage::exists("private/$path")) {
+        abort(404);
+    }
+
+    // Return the PDF with the correct response headers
+    return response()->file(storage_path("app/private/$path"), [
+        'Content-Type' => 'application/pdf',
+        'Content-Disposition' => 'inline; filename="' . basename($path) . '"',  // Ensures the PDF opens in browser
+    ]);
+});
 
 
 
